8e843b16e771285897533c85a8f80b3e05ed52b5
John Beck <john.m.beck@gmail.com>
2016-08-10 13:09:56 -0700

    Fixes compile error

--- Source/SwiftyJSON.swift
+++ Source/SwiftyJSON.swift
@@ -736,7 +736,7 @@ extension JSON {
             switch self.type {
             case .string:
                 let decimal = NSDecimalNumber(string: self.object as? String)
-                if decimal == NSDecimalNumber.notA {  // indicates parse error
+                if decimal == NSDecimalNumber.notANumber {  // indicates parse error
                     return NSDecimalNumber.zero
                 }
                 return decimal
